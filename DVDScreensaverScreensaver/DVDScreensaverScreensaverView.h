e7515fc7087683febac4d2f26e9843d6feeadb58
aryamansharda <asharda@turo.com>
2024-11-12 19:41:43 -0800

    Initial Commit

---
+++ DVDScreensaverScreensaver/DVDScreensaverScreensaverView.h
@@ -0,0 +1,12 @@
+//
+//  DVDScreensaverScreensaverView.h
+//  DVDScreensaverScreensaver
+//
+//  Created by Aryaman Sharda on 11/12/24.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface DVDScreensaverScreensaverView : ScreenSaverView
+
+@end
